dd886b7eeeb60665508ac1541057b2952a8c5dbf
lilrichy <richardreigens@gmail.com>
2015-11-18 17:55:12 -0500

    v1.3

--- DwSounds/DwSoundsCode.lua
+++ DwSounds/DwSoundsCode.lua
@@ -7,7 +7,7 @@ A small collection of Sounds that are used for different spells of different cla
 local appName = "|cFF0000FFDwSounds:|r ";
 local DwSoundsFrame = CreateFrame("FRAME") DwSoundsFrame:Hide()
 
-local _debug = false
+local _debug = false -- Enable to Display debug messages.
 function errTxt(msg)
 	if ( _debug == true ) then
 		print(appName.. " _DEBUG : "..msg);
@@ -22,6 +22,10 @@ function DwSoundsFrame:ADDON_LOADED(arg1)
 		print(appName.. "Loaded");
 	
 end
+
+function DwSoundsFrame:PLAYER_DEAD() -- Sound for when Player Dies
+	PlaySoundFile("Sound\\Creature\\Kologarn\\UR_Kologarn_Slay02.ogg", "Master")
+	end
 
 function DwSoundsFrame:UNIT_SPELLCAST_SUCCEEDED(unitID, spell, rank, lineID, spellID)
 	if unitID == "player" then
@@ -41,12 +45,18 @@ function DwSoundsFrame:UNIT_SPELLCAST_SUCCEEDED(unitID, spell, rank, lineID, spe
 					PlaySoundFile("Interface\\AddOns\\DwSounds\\Sounds\\GetOverHere.ogg", "Master")
 					errTxt("Sound Played");
 				end
+				
+				if spellID == 42650 then -- Army of the Dead
+					errTxt("Army of the Dead Detected");
+					PlaySoundFile("Sound\\Creature\\HeadlessHorseman\\Horseman_Special_01.ogg", "Master")
+					errTxt("Sound Played");
+				end
 	-- End Death Knight Sounds
 	
 	-- Begin Warrior Sounds
 				if spellID == 46924 then -- Blade Storm
 					errTxt("Blade Storm Detected");
-					PlaySoundFile("Interface\\AddOns\\DwSounds\\Sounds\\BoneStorm.ogg", "Master")
+					PlaySoundFile("Sound\\Creature\\LordMarrowgar\\IC_Marrowgar_WW01.ogg", "Master")
 					errTxt("Sound Played");
 				end
 	-- End Warrior Sounds
@@ -57,3 +67,4 @@ end
 DwSoundsFrame:SetScript("OnEvent", function(self, event, ...) return self[event](self, ...) end)
 DwSoundsFrame:RegisterEvent("ADDON_LOADED")
 DwSoundsFrame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+DwSoundsFrame:RegisterEvent("PLAYER_DEAD")
